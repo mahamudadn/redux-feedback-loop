279e6ccfebcb3156027529e1a77e4a7b8f549f3b
Mahamud Aden <mahamudadn@gmail.com>
2023-06-09 19:10:38 -0500

    added a post from router side

--- server/Router/Router.Feedback.js
+++ server/Router/Router.Feedback.js
@@ -14,7 +14,22 @@ router.get('/', (req, res) => {
 })
 
 
+router.post('/', (req, res) => {
+    let newFeedback = req.body
+    console.log('adding a feedback', newFeedback);
+    let sqlText = `INSERT INTO "feedback"("feeling", "understanding", "support", "comments")
+        VALUES ($1, $2, $3, $4);`
+    pool.query(sqlText, [newFeedback.feeling, newFeedback.understanding, newFeedback.support, newFeedback.comments])
+        .then(result => {
+            console.log('Succesfully Postes');
+            res.sendStatus(201)
+        })
+        .catch(error => {
+            console.log('error in posting', error);
+            res.sendStatus(500)
+        })
 
+})
 
 
 
